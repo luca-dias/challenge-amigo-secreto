0dc7a6879adca95b7d415fd96899d5024a7eb935
Luca Dias <luca_dias1507@hotmail.com>
2025-08-09 15:25:54 -0300

    criada função para adicionar nomes na lista

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo");
+    if (nome.value != ''){
+        nomes.push(nome.value);
+        // console.log(nomes);
+    } else {
+        alert("Insira um nome válido!");
+    }
+    nome.value = '';
+}
